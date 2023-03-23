f07dad3773a6724ad1b0f3c75c2ad7cc83272a5d
Nathbobs <46452812+Nathbobs@users.noreply.github.com>
2023-03-23 23:19:51 +0900

    ..

--- Section 6/NumberInWordChallenge/src/NumberInWord.java
+++ Section 6/NumberInWordChallenge/src/NumberInWord.java
@@ -1,3 +1,12 @@
+/* Number In Word
+Write a method called printNumberInWord. The method has one parameter number which is the whole number.
+The method needs to print "ZERO", "ONE", "TWO", ... "NINE", "OTHER"
+if the int parameter number is 0, 1, 2, .... 9 or other for any other number including negative numbers.
+You can use if-else statement or switch statement whatever is easier for you.
+NOTE: Method printNumberInWord needs to be public static for now, we are only using static methods.
+
+*/
+
 public class NumberInWord {
     public static void main(String[] args) {
 
@@ -15,7 +24,7 @@ public class NumberInWord {
         printNumberInWord(-1);
 
     }
-    public static int printNumberInWord(int num){
+    public static void printNumberInWord(int num){
         switch(num){
             case 0:
                 System.out.println("Zero");
@@ -50,6 +59,6 @@ public class NumberInWord {
             default:
                 System.out.println("OTHER");
 
-        } return num;
+        }
     }
 }
